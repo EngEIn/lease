321149b479d4166db130fd5399996846c88a190d
engin <egeini@163.com>
2024-08-12 17:55:17 +0800

    公寓管理相关代码完善

--- web/web-admin/src/main/java/com/engein/lease/web/admin/AdminWebApplication.java
+++ web/web-admin/src/main/java/com/engein/lease/web/admin/AdminWebApplication.java
@@ -4,11 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 
 @SpringBootApplication
 @MapperScan("com.engein.lease.web.*.mapper")
-@ComponentScan("com.engein.lease.common.minio")
+@ComponentScans({@ComponentScan("com.engein.lease.common.minio"),@ComponentScan("com.engein.lease.web.admin.custom.config")})
 public class AdminWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminWebApplication.class, args);
